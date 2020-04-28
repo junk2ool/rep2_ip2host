6ae78c0f07521f049405a423e4285f64340270b6
junk2ool <junk2ool@outlook.jp>
2020-04-28 10:23:04 +0900

    beta 1

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
 # rep2_ip2host
+
+rep2(open774版)で名前欄にIPアドレスがあれば逆引きをしてホスト名に書き換えるもの。  
+非同期でホスト名の逆引きをして書き換えるのでスレが表示されてから書き変わるまでラグがあります。  
+HTML5のWeb Storageを使ってブラウザ側に逆引き結果をキャッシュをするようにしてあるので2回目以降キャッシュにあれば少し早いです。  
+Web Storage対応のブラウザでない場合はrep2サーバーの/p2-php/data/tmp/にキャッシュファイルを作ります。  
+また、逆引きしたホスト名にrep2の名前欄のあぼーん処理をかけて該当すれば透明あぼーんします。  
+逆引きはrep2サーバー側で行い、書き換えはブラウザ側のjQueryで動的に行っているためhtmlソースには反映されません。
+
+## Install
+  * lib/ip2host.inc.php  
+  * rep2/ip2host.php
+ 
+をそれぞれの環境の/p2-phpの中に入れてください。  
+その後、  
+  * lib/read_footer.inc.php (PC用)
+  * lib/read_footer_i.inc.php (スマホ用)
+
+の最後方の部分に、
+```php
+// ここから
+require P2_LIB_DIR . '/ip2host.inc.php';
+// ここまで
+
+// ====
+echo '</body></html>';
+```
+を追記してください。
+
+## Environment
+Linux(CentOS)のPHP7+Apacheで動作確認しています。
+
+## Note
+### Cache
+キャッシュはHTML5のWeb Storage(sessionStorage)を使用しています。  
+そのため別タブ、ブラウザの再起動などでキャッシュは消えます。  
+localStorageが使用でき、キャッシュをブラウザ側に半永久的に保持する場合はip2host.inc.phpの
+```JavaScript
+st = sessionStorage;
+//  LocalStorageを使う場合は下記のコメントアウトを解除
+//st = localStorage;
+```
+のコメントアウトを解除してください。  
+その場合のWeb Storageのキャッシュの削除の方法は「Web Storage 削除」でぐぐれば出てきます。  
+なお、キャッシュは500件を超えるとクリアするようにしています。  
+500件を変更する場合はip2host.inc.phpの
+```JavaScript
+//  キャッシュするIPの上限数
+let ip_cache_size = 500;
+```
+を変更してください。  
+
+キャッシュをサーバー側のファイルに作る場合は500件を超えると古いものから10件づつ削除します。  
+常にキャッシュをサーバー側のファイルで使用する場合（ブラウザ側にキャッシュを持たせたくない、複数のブラウザで使う等）はip2host.inc.phpの
+```JavaScript
+//  常にキャッシュをサーバー側のファイルで使用する場合は下記のコメントアウトを解除
+//web_storage = 0;
+```
+のコメントアウトを解除してください。
+
+### Aborn
+ホスト名の名前欄でのあぼーん処理が不要な場合はip2host.inc.phpの
+```JavaScript
+// 名前欄のあぼーん処理を適用 する:1 しない:0
+let aborn = 1;
+```
+を変更してください。  
+ちょっと早くなると思います。
+
+### Replace
+非同期の書き換え処理には念の為タイムアウトを設定してあるので、サーバーや回線、ブラウザのスペックにより大量のレス(1-1000とか)を表示するとすべて書き変わらないことがあります。  
+その場合はタイムアウトの秒数を変更するなりしてみてください。  
+
+画面スクロール時に表示されている部分のみ随時書き換えるように変更する場合は、
+```JavaScript
+//  スレ表示時に一括書き換え:0 画面スクロールで書き換え:1
+let scroll_replace = 0;
+```
+を変更してください。  
+
+## ToDo
+スマホ用ポップアップもちゃんと書き換えたい。  
+(一応ポップアップさせた状態でスクロールをすると書き換わります。)
+
+## License
+[MIT License](https://mit-license.org/)
+
+## Author
+* junk2ool  
+https://github.com/junk2ool/rep2_ip2host
+
+## Reference
+* rep2 expack 全部入り by open774  
+https://github.com/open774/p2-php
---
+++ lib/ip2host.inc.php
@@ -0,0 +1,151 @@
+<?php
+
+// {{{ ip2host.inc.php
+
+/**
+* ip2host
+*/
+
+//  ip->hostしてあぼーん
+echo <<<IP2HOST
+<script>
+    let web_storage = 0;
+
+    //  スレ表示時に一括書き換え:0 画面スクロールで書き換え:1
+    let scroll_replace = 0;
+
+    if (('sessionStorage' in window) && (window.sessionStorage !== null)) {
+        web_storage = 1;
+    }
+    //  常にキャッシュをサーバー側のファイルで使用する場合は下記のコメントアウトを解除
+    //web_storage = 0;
+
+    //$(window).on('load',function() {
+    $(function() {
+        ip2host(scroll_replace, web_storage);
+    });
+
+    if (scroll_replace) {
+        $(window).on('scroll click', function () {
+            ip2host(scroll_replace, web_storage);
+        });
+    }
+
+    function ip2host(scroll_replace, web_storage = 0) {
+        let st = null;
+
+        if (web_storage) {
+            st = sessionStorage;
+            //  LocalStorageを使う場合は下記のコメントアウトを解除
+            //st = localStorage;
+        }
+
+        $('div.res-header').each(function() {
+            let res_header = $(this);
+
+            let header_top = res_header.offset().top;
+            let scroll_top = $(window).scrollTop();
+            let wh = $(window).height();
+            if (!scroll_replace || (scroll_replace && header_top > scroll_top && header_top < scroll_top + wh)) {
+                let res_header_tmp = res_header.clone();
+                res_header_tmp.find('b').text('');
+                let ip_addr = res_header_tmp.html().match(/.+? \[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})[\]| ]/);
+                if (ip_addr == null) {
+                    return true;
+                };
+
+                replace_html(ip_addr[1], st, res_header);
+            }
+        });
+    }
+
+    function replace_html(ip_addr, st, res_header) {
+        let param = {};
+        let ip2host_url = 'ip2host.php';
+        let host = null;
+
+        //  名前欄のあぼーん処理を適用 する:1 しない:0
+        let aborn = 1;
+
+        //  キャッシュするIPの上限数
+        let ip_cache_size = 500;
+
+        if (st) {
+            if (st.length > ip_cache_size) {
+                st.clear();
+            } else {
+                host = st.getItem(ip_addr);
+            }
+        } else {
+            param['cache_size'] = ip_cache_size;
+		}
+        if (host === null) {
+            param['action'] = 'GetHost';
+        } else {
+            param['action'] = 'AbornHost';
+        }
+        param['ip'] = ip_addr;
+        param['aborn'] = aborn;
+        param['host'] = host;\n
+IP2HOST;
+        echo "            param['bbs'] = '".$bbs."';\n";
+        echo "            param['title'] = '".$ttitle_en."';\n";
+echo <<<IP2HOST
+        
+        $.ajax({
+            type: "GET",
+            url: ip2host_url,
+            data: param,
+            dataType : "text",
+            timeout: 30000
+        }).done(function(data){
+            let ret_host = $.trim(data);
+
+            if (host === null) {
+                if (ret_host.indexOf(',') != -1) {
+                    let ret = ret_host.split(',');
+
+                    host = ret[0];
+                    ret_host = ret[1];
+                }
+                else
+                {
+                    host = ret_host;
+                }
+
+                if (st) {
+                    st.setItem(ip_addr, host);
+                }
+            }
+
+            if (ret_host == ip_addr) {
+                console.log('unresolved IP address: '+ret_host);
+            } else if (ret_host == 'aborn'){
+                res_header.hide();
+                res_header.parent().find('.message').hide();
+                res_header.parent().hide();
+            } else {
+                let b_text = res_header.find('b').text();
+                res_header.find('b').text('');
+                res_header.html(res_header.html().replace(/ \[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})([\]| ])/, ' ['+ret_host+'$2'));
+                res_header.find('b').text(b_text);
+            }
+        }).fail(function(XMLHttpRequest, textStatus, errorThrown){
+            //alert(errorThrown);
+        });
+	}
+</script>\n
+IP2HOST;
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: cp932
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
+// vim: set syn=php fenc=cp932 ai et ts=4 sw=4 sts=4 fdm=marker:
